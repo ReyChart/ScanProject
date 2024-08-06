97d996d39166fc09f80fd0eb5fa1d4f50ba4295e
ReyChart <reycharton@gmail.com>
2024-08-06 14:10:31 +0300

    Added styles for footer and made it adaptive

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './Footer.module.scss';
 
@@ -6,9 +7,25 @@ const Footer: FunctionComponent = () => {
   return (
     <footer className={styles.footer}>
       <div className="container">
-        <p>
-          Footer СКАН, <span>{new Date().getFullYear()}</span>
-        </p>
+        <Link to="/" className={styles.footerLogo}>
+          <img src="./scanBurgerLogo.svg" alt="Logo" />
+        </Link>
+        <div className={styles.infoWrapper}>
+          <p className={styles.text}>г. Москва, Цветной б-р, 40</p>
+          <p className={styles.text}>
+            <a className={styles.link} href="tel:+74957712111">
+              +7 495 771 21 11
+            </a>
+          </p>
+          <p className={styles.text}>
+            <a className={styles.link} href="mailto:info@skan.ru">
+              info@skan.ru
+            </a>
+          </p>
+          <p className={styles.text}>
+            Copyright. <span>{new Date().getFullYear()}</span>
+          </p>
+        </div>
       </div>
     </footer>
   );
